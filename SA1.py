db7760907311012d09e91541f7b149743e1f48f3
brunasilva157 <bruna_silva157@estudante.sesisenai.org.br>
2026-02-19 19:16:32 -0300

    Create SA1.py

---
+++ SA1.py
@@ -0,0 +1,12 @@
+nome_site = ("SpieceGirls")
+categoria = ("Streaming")
+catalogo = (2000)
+nome_acinante = ("\nRicardo\nGustavo\nAngélica\nPedro\nJulio")
+maior_de_idade = True
+assinatura = (29.90)
+generos = ("\nAção\nAventura\nTerror\nAnimação\nComédia\nSuspense")
+servidor = ("\nÁmericas\nEuropa\nAsia\nAfrica\nOceania")
+
+site = f"Bem vindo ao {nome_site}!\n O seu servidor de {categoria} :)\nCatalogo vasto com mais de {catalogo} titulos :o\nNossos assinantes premium:{nome_acinante}\nÉ maior de idade? {maior_de_idade}\nAssinatura por apenas: {assinatura}!!!\nCom as categorias mais queridinhas:{generos}=)\nCom servidores espalhados por todo mundo: {servidor}!!"
+
+print (site)

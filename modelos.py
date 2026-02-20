7963e112101902f294486f7ab0b3ee4a5781fdb8
brunasilva157 <bruna_silva157@estudante.sesisenai.org.br>
2026-02-19 21:38:42 -0300

    Update modelos.py

--- modelos.py
+++ modelos.py
@@ -1,12 +1,7 @@
-nome_site = ("SpieceGirls")
-categoria = ("Streaming")
-catalogo = (2000)
-nome_acinante = ("\nRicardo\nGustavo\nAngélica\nPedro\nJulio")
-maior_de_idade = True
-assinatura = (29.90)
-generos = ("\nAção\nAventura\nTerror\nAnimação\nComédia\nSuspense")
-servidor = ("\nÁmericas\nEuropa\nAsia\nAfrica\nOceania")
+"""
+Módulo modelos.py
+Definição das classes do sistema.
 
-site = f"Bem vindo ao {nome_site}!\n O seu servidor de {categoria} :)\nCatalogo vasto com mais de {catalogo} titulos :o\nNossos assinantes premium:{nome_acinante}\nÉ maior de idade? {maior_de_idade}\nAssinatura por apenas: {assinatura}!!!\nCom as categorias mais queridinhas:{generos}=)\nCom servidores espalhados por todo mundo: {servidor}!!"
-
-print (site)
+Criado em: 19/02/2026
+Autor: Bruna
+"""
